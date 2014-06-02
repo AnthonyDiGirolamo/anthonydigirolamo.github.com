318260c4c5f8c9e0f6a46b28fa63296b9507a9de
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-06-02 14:19:14 -0400

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 bundle exec jekyll build
 git checkout master
-rsync -rlpgoDv --delete -I --ignore-existing .git --ignore-existing vendor --ignore-existing _site --ignore-existing .bundle --ignore-existing .gitignore --ignore-existing .nojekyll --ignore-existing .projectile --ignore-existing .rbenv-version _site/ ./
-# git add -A .
-# git commit -m 'jekyll build'
+cp -r _site/* ./
+rsync -av --delete --ignore-times --ignore-existing .git --ignore-existing vendor --ignore-existing _site --ignore-existing .bundle --ignore-existing .gitignore --ignore-existing .nojekyll --ignore-existing .projectile --ignore-existing .rbenv-version _site/ ./
+git add -A .
+git commit -m 'jekyll build'
